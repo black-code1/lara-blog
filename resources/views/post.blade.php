6a396f3ce9f22860a2e46257bf3d07b3e37fb938
black-code1 <legrandmahamat1@gmail.com>
2022-06-17 11:37:14 +0100

    Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
+        </p>
+    </article>
+</body>
