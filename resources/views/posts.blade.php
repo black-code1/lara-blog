87eee2f51c74cb6689617d93e059d848abd09272
black-code1 <legrandmahamat1@gmail.com>
2022-06-18 14:09:25 +0100

    Find a Composer Package for Post Metadata

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,25 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/post/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error inventore non numquam quis soluta unde. Amet at ducimus ex expedita iste neque nihil provident quia reprehenderit! Aut cumque delectus dolores expedita illum ipsum maiores officiis saepe tempore tenetur. Accusantium aliquam architecto aut commodi, cupiditate id impedit, in iure iusto odio odit officiis, pariatur placeat possimus quam repellat ullam vitae voluptatibus.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
