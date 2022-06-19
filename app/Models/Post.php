27c96e7d07c7d95414a1f2048914298df1f017b5
black-code1 <legrandmahamat1@gmail.com>
2022-06-19 01:08:32 +0100

    Blade: The Absolute Basics

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -37,15 +36,19 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts/")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn ($document) =>  new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug,
-            ));
+        return cache()->rememberForever('posts.all', function(){
+            return collect(File::files(resource_path("posts/")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) =>  new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug,
+                ))
+                ->sortByDesc('date');
+        });
+
     }
     public static function find($slug)
     {
