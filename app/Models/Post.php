1d4705694af9803dacd246814cee25ed4e229f95
black-code1 <legrandmahamat1@gmail.com>
2022-06-20 05:09:31 +0100

    A Few Tweaks and Consideration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -54,5 +55,16 @@ class Post
     {
         // of all the blog posts, find the one with a slug that matches the one that was requested.
         return static::all()->firstWhere('slug', $slug);
+    }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if(!$post){
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 }
