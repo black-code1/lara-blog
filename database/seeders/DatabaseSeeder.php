ee69d338bd66bc8cd84e5fee1247b4ff03fd38e7
black-code1 <legrandmahamat1@gmail.com>
2022-06-24 16:24:44 +0100

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +17,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::truncate();
+         Post::truncate();
+         Category::truncate();
+
+         $user = User::factory()->create();
+
+         $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+         ]);
+
+         $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+         $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+         Post::create([
+             'user_id' => $user->id,
+             'category_id' => $family->id,
+             'title' => 'My Family Post',
+             'slug' => 'my-first-post',
+             'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
+         ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
