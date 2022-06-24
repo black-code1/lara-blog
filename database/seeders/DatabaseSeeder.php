ed9f09f54bbbb2de79ceb0d0430978bd91cef359
black-code1 <legrandmahamat1@gmail.com>
2022-06-24 17:19:48 +0100

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,44 +17,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::truncate();
-         Post::truncate();
-         Category::truncate();
-
-         $user = User::factory()->create();
-
-         $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+//         User::truncate();
+//         Post::truncate();
+//         Category::truncate();
+         $user = User::factory()->create([
+             'name' => 'John Doe'
+         ]);
+         Post::factory(5)->create([
+             'user_id' => $user->id
          ]);
 
-         $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
+//         $user = User::factory()->create();
 
-         $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-         Post::create([
-             'user_id' => $user->id,
-             'category_id' => $family->id,
-             'title' => 'My Family Post',
-             'slug' => 'my-first-post',
-             'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
-         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
-        ]);
+//         $personal = Category::create([
+//            'name' => 'Personal',
+//            'slug' => 'personal'
+//         ]);
+//
+//         $family = Category::create([
+//            'name' => 'Family',
+//            'slug' => 'family'
+//        ]);
+//
+//         $work = Category::create([
+//            'name' => 'Work',
+//            'slug' => 'work'
+//        ]);
+//
+//         Post::create([
+//             'user_id' => $user->id,
+//             'category_id' => $family->id,
+//             'title' => 'My Family Post',
+//             'slug' => 'my-first-post',
+//             'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+//             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
+//         ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $work->id,
+//            'title' => 'My Work Post',
+//            'slug' => 'my-work-post',
+//            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+//            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda corporis deleniti deserunt dolor dolorem eligendi enim, et exercitationem facere illo illum maiores natus necessitatibus saepe soluta tempora tempore tenetur vero. Aut delectus dignissimos doloremque doloribus, eveniet explicabo fuga harum in ipsa nam nihil pariatur perferendis perspiciatis, porro rem saepe sequi tenetur veritatis, vero voluptate. Distinctio impedit nobis voluptas.</p>'
+//        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
