f4a07f0d0b47136258ef4e1ab4830722ccd340e7
black-code1 <legrandmahamat1@gmail.com>
2022-06-27 16:23:43 +0100

    Build a Register User Page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\Category;
@@ -8,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
