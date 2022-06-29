49cc32e336cffa71026ae7f11a2c36c558cac2cb
black-code1 <legrandmahamat1@gmail.com>
2022-06-29 05:45:47 +0100

    Design the Comment Form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,4 +1,7 @@
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+@props(['comment'])
+
+<x-panel class="bg-gray-10">
+    <article class="flex space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
     </div>
@@ -18,3 +21,4 @@
         </p>
     </div>
 </article>
+</x-panel>
