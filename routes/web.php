5a873dd0440ecf531376573b9e6b343ddd98389f
black-code1 <legrandmahamat1@gmail.com>
2022-06-29 15:29:47 +0100

    Mailchimp API Tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function (){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us12'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("ba1d906fb3", [
+        'email_address' => 'legrandmahamat@gmail.com',
+        'status' => 'subscribed',
+    ]);
+
+
+    ddd($response);
+});
+
 Route::get('/',[PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
